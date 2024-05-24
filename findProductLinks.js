0faeab27d2432162c91bf3e359539766a6731982
ranjbarmahdi <ranmahdi66@gmail.com>
2024-05-24 19:10:40 +0330

    a

--- findProductLinks.js
+++ findProductLinks.js
@@ -44,11 +44,34 @@ async function findAllMainLinks(page, initialUrl) {
         const $ = cheerio.load(html);
 
         // Getting All Main Urls In This Page
-        const mainLinks = $('notFound')
-            .map((i, a) => $(a).attr('href')?.trim()).get()
+        const mainLinks = [
+            'https://digipalizi.com/shop/?swoof=1&pa_product-type=%D8%A2%DB%8C%D9%86%D9%87&paged=1',
+            'https://digipalizi.com/shop/?swoof=1&paged=1&pa_product-type=%D9%BE%D8%A7%D8%B1%DA%A9%D8%AA',
+            'https://digipalizi.com/shop/?swoof=1&paged=1&pa_product-type=%D9%84%D9%85%DB%8C%D9%86%D8%AA',
+            'https://digipalizi.com/shop/?swoof=1&paged=1&pa_product-type=%D9%BE%D8%A7%D9%81',
+            'https://digipalizi.com/shop/?swoof=1&paged=1&pa_product-type=%D9%BE%D8%B1%D8%AF%D9%87',
+            'https://digipalizi.com/shop/?swoof=1&paged=1&pa_product-type=%D9%BE%D8%B1%D9%88%D9%81%DB%8C%D9%84',
+            'https://digipalizi.com/shop/?swoof=1&paged=1&pa_product-type=%D9%BE%D9%88%D8%B3%D8%AA%D8%B1',
+            'https://digipalizi.com/shop/?swoof=1&paged=1&pa_product-type=%DA%86%D8%B3%D8%A8',
+            'https://digipalizi.com/shop/?swoof=1&paged=1&pa_product-type=%DA%86%D9%85%D9%86-%D9%85%D8%B5%D9%86%D9%88%D8%B9%DB%8C',
+            'https://digipalizi.com/shop/?swoof=1&paged=1&pa_product-type=%DA%86%D9%88%D8%A8-%D9%BE%D9%84%D8%A7%D8%B3%D8%AA',
+            'https://digipalizi.com/shop/?swoof=1&paged=1&pa_product-type=%D8%AF%DB%8C%D9%88%D8%A7%D8%B1%D9%BE%D9%88%D8%B4',
+            'https://digipalizi.com/shop/?swoof=1&paged=1&pa_product-type=%D8%B1%D9%86%DA%AF',
+            'https://digipalizi.com/shop/?swoof=1&paged=1&pa_product-type=%D8%B1%D9%88%D9%BE%D9%84%D9%87-%D8%A7%DB%8C',
+            'https://digipalizi.com/shop/?swoof=1&paged=1&pa_product-type=%D8%B3%D9%82%D9%81-%DA%A9%D8%A7%D8%B0%D8%A8',
+            'https://digipalizi.com/shop/?swoof=1&paged=1&pa_product-type=%D8%B4%D8%A7%D9%85%D9%BE%D9%88',
+            'https://digipalizi.com/shop/?swoof=1&paged=1&pa_product-type=%D8%B5%D9%86%D8%AF%D9%84%DB%8C',
+            'https://digipalizi.com/shop/?swoof=1&paged=1&pa_product-type=%D9%81%D9%88%D9%85',
+            'https://digipalizi.com/shop/?swoof=1&paged=1&pa_product-type=%DA%A9%D8%A7%D8%BA%D8%B0-%D8%AF%DB%8C%D9%88%D8%A7%D8%B1%DB%8C',
+            'https://digipalizi.com/shop/?swoof=1&paged=1&pa_product-type=%DA%A9%D9%81%D9%BE%D9%88%D8%B4',
+            'https://digipalizi.com/shop/?swoof=1&paged=1&pa_product-type=%D9%85%D9%88%DA%A9%D8%AA',
+            'https://digipalizi.com/shop/?swoof=1&paged=1&pa_product-type=%D9%85%D9%81%D8%B1%D8%B4',
+            'https://digipalizi.com/shop/?swoof=1&paged=1&pa_product-type=%D9%85%DB%8C%D8%B2',
+            'https://digipalizi.com/shop/?swoof=1&paged=1&pa_product-type=%D9%88%D8%A7%DA%A9%D8%B3',
+        ]
 
         // Push This Page Products Urls To allProductsLinks
-        allMainLinks.push(initialUrl);
+        allMainLinks.push(...mainLinks);
 
     } catch (error) {
         console.log("Error In findAllMainLinks function", error.message);
